6a86b30a0d40f1f73105a2ba46a61b533112d025
Bakaev Nikita <ya@nbakaev.ru>
2014-03-18 20:47:06 +0400

    more clear

--- CParser/Parser.cpp
+++ CParser/Parser.cpp
@@ -3,77 +3,14 @@
 #include "SimpleList.h"
 #include <vector>
 #include <iostream>
-
 #include <winsock2.h>
 #include <windows.h>
 #include <iostream>
 #pragma comment(lib,"ws2_32.lib")
 
 
-
-#include <curl/curl.h>
-
-//#include 
-//#include <stdio.h>
-
-// Curl
-//#include <curlpp/curlpp.hpp>
-//#include <curlpp/Easy.hpp>
-//#include <curlpp/Options.hpp>
-//
-
-
-
-
-
 using namespace std;
-//
-//using namespace curlpp;
-//using namespace curlpp::options;
 
-
-
-
-static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
-{
-	((std::string*)userp)->append((char*)contents, size * nmemb);
-	return size * nmemb;
-}
-
-
-//string CParser::getBody(){
-//
-//	regex bodyRegExp("(<body>[^]*</body>)");
-//
-//	string str = this->html;
-//	sregex_iterator it(str.begin(), str.end(), bodyRegExp);
-//	sregex_iterator it_end;
-//	return it->str();
-//}
-
-
-
-//auto CParser::getByTagWithContext(string tag,string html) -> vector<string>{
-//
-//	//| <" + tag + "[^]*/>
-//	regex bodyRegExp("(<" + tag + " (.*)" + ">(.*)</" + tag + ">) | (<" + tag + "(.*)/>)");
-//
-//		string str = this->html;
-//		sregex_iterator it(str.begin(), str.end(), bodyRegExp);
-//		sregex_iterator it_end;
-//
-//		vector<string> s;
-//
-//		while (it != it_end){
-//	//cout << "|||New expression||||";
-//		//list.append(it->str());
-//			
-//	s.push_back(it->str());
-//			++it;
-//		}
-//
-//	return s;
-//}
 
 
 
@@ -81,6 +18,9 @@ auto CParser::getByTag(string tag) -> vector<CNode*>{
 
 	vector<CNode*> result;
 	vector<string> tagToFind;
+
+
+	//TODO
 	// "tag1 tag2 tag3"
 
 	// govnokod //
@@ -124,62 +64,6 @@ auto CParser::getByTag(string tag) -> vector<CNode*>{
 
 
 
-
-
-//
-//auto CParser::makeDOM() -> void{
-//	
-//	////////////////////
-//	
-//	//| <" + tag + "[^]*/>
-//
-//	string tag = "div";
-//
-//	regex bodyRegExp("(<" + tag + " (.*)" + ">(.*)</" + tag + ">) | (<" + tag + "(.*)/>)");
-//
-//	string str = this->html;
-//	sregex_iterator it(str.begin(), str.end(), bodyRegExp);
-//	sregex_iterator it_end;
-//
-//	vector<string> s;
-//
-//	while (it != it_end){
-//
-//		////////// Here are all html tags
-//
-//
-//
-//
-//		//cout << "|||New expression||||";
-//		//list.append(it->str());
-//
-//		//s.push_back(it->str());
-//
-//		cout << it->str();
-//
-//		++it;
-//	}
-//
-//
-//	///////////////
-//
-//
-//
-//
-//
-//
-//	if (this->rootNode == nullptr){
-//
-//		CNode* myRootNode = this->getPointerByTag("html");
-//
-//	}
-//
-//
-//
-//
-//
-//
-//}
 
 
 auto CParser::makeDOM() -> void{
@@ -284,11 +168,7 @@ auto CParser::makeDOM() -> void{
 }
 
 
-
-
 /////////////////////////////////////////////////////////////////   Not Interesring
-
-
 
 
 CParser::CParser(string url){
@@ -319,22 +199,13 @@ CParser::CParser(string url){
 			for (int i = 0; i < hostname.length(); i++){
 				if (hostname[i] != '/') { t += hostname[i]; }
 				else{
-					//for (j = 0; j < hostname.le)
 					suburl = hostname.substr(t.length() +1);
-
 					break;
-
 				}
-
 			}
 
 			/// t - real hostname ( "localhost")
 			/// suburl - all after / "localhost/blabla"
-
-
-
-		//string hostname = "ibakaev.ru";
-
 
 		SOCKET Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 		struct hostent *host;
@@ -343,20 +214,11 @@ CParser::CParser(string url){
 		SockAddr.sin_port = htons(80);
 		SockAddr.sin_family = AF_INET;
 		SockAddr.sin_addr.s_addr = *((unsigned long*)host->h_addr);
-	//	cout << "Connecting...\n";
 		if (connect(Socket, (SOCKADDR*)(&SockAddr), sizeof(SockAddr)) != 0){
 			cout << "Could not connect";
-	//		system("pause");
-//			return 1;
 		}
-//		cout << "Connected.\n";
-
-
 		string result;
-
 		string s1 = "GET /"+suburl+" HTTP/1.1\r\nHOST: localhost\r\nConnection: close\r\n\r\n";
-
-		//string s1 = "GET / HTTP 1.1\r\nHost: "+hostname+"\rConnection: close\r\n\r\n";
 
 		send(Socket, s1.c_str(), strlen(s1.c_str()), 0);
 		char buffer[10];
@@ -371,31 +233,7 @@ CParser::CParser(string url){
 		}
 		closesocket(Socket);
 		WSACleanup();
-
-		//this->html = result;
-
 		url = result;
-
-		//url = result;
-
-		//CURL *curl;
-		//CURLcode res;
-		//std::string readBuffer;
-
-		//curl = curl_easy_init();
-		//if (curl) {
-		//	curl_easy_setopt(curl, CURLOPT_URL, url);
-		//	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-		//	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-		//	res = curl_easy_perform(curl);
-		//	curl_easy_cleanup(curl);
-
-		//	//return readBuffer;
-
-		//	url = readBuffer;
-
-		//	//url = this->getHTMLfromHTTP(url);
-		//}
 	}
 
 	url.erase(url.find_last_not_of(" \n\r\t") + 1); // trim
@@ -404,16 +242,6 @@ CParser::CParser(string url){
 	this->makeDOM();
 
 }
-
-//
-//sregex_iterator CParser::getNullIterator(){
-//	return this->iteratorEnd;
-//}
-
-
-
-
-
 
 CParser::CParser()
 {
@@ -424,71 +252,98 @@ CParser::~CParser()
 {
 }
 
- auto getContentFromURL(string url)->string{
-	//try
-	//{
-	//	// That's all that is needed to do cleanup of used resources
-	//	curlpp::Cleanup myCleanup;
-
-	//	// Our request to be sent.
-	//	curlpp::Easy myRequest;
-
-	//	// Set the URL.
-	//	myRequest.setOpt<Url>("http://example.com");
-
-	//	// Send request and get a result.
-	//	// By default the result goes to standard output.
-	//	myRequest.perform();
-	//}
-
-	//catch (curlpp::RuntimeError & e)
-	//{
-	//	std::cout << e.what() << std::endl;
-	//}
-
-	//catch (curlpp::LogicError & e)
-	//{
-	//	std::cout << e.what() << std::endl;
-	//}
 
 
-	//CURL *curl;
-	//CURLcode res;
-
-	//curl = curl_easy_init();
-	//if (curl) {
-	//	curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
-	//	res = curl_easy_perform(curl);
-
-
-	//	return curl_easy_perform();
-
-	//	/* always cleanup */
-	////	curl_easy_cleanup(curl);
-	//
-	//
-	//}
-
-
-	//CURL *curl;
-	//CURLcode res;
-	//std::string readBuffer;
-
-	//curl = curl_easy_init();
-	//if (curl) {
-	//	curl_easy_setopt(curl, CURLOPT_URL, url);
-	//	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-	//	curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-	//	res = curl_easy_perform(curl);
-	//	curl_easy_cleanup(curl);
-
-	//	return readBuffer;
-
-	//	/*std::cout << readBuffer << std::endl;*/
-	//}
-
-
-}
+///////////// ===================== TRASH
 
 
 
+//
+//auto CParser::makeDOM() -> void{
+//	
+//	////////////////////
+//	
+//	//| <" + tag + "[^]*/>
+//
+//	string tag = "div";
+//
+//	regex bodyRegExp("(<" + tag + " (.*)" + ">(.*)</" + tag + ">) | (<" + tag + "(.*)/>)");
+//
+//	string str = this->html;
+//	sregex_iterator it(str.begin(), str.end(), bodyRegExp);
+//	sregex_iterator it_end;
+//
+//	vector<string> s;
+//
+//	while (it != it_end){
+//
+//		////////// Here are all html tags
+//
+//
+//
+//
+//		//cout << "|||New expression||||";
+//		//list.append(it->str());
+//
+//		//s.push_back(it->str());
+//
+//		cout << it->str();
+//
+//		++it;
+//	}
+//
+//
+//	///////////////
+//
+//
+//
+//
+//
+//
+//	if (this->rootNode == nullptr){
+//
+//		CNode* myRootNode = this->getPointerByTag("html");
+//
+//	}
+//
+//
+//
+//
+//
+//
+//}
+
+
+//string CParser::getBody(){
+//
+//	regex bodyRegExp("(<body>[^]*</body>)");
+//
+//	string str = this->html;
+//	sregex_iterator it(str.begin(), str.end(), bodyRegExp);
+//	sregex_iterator it_end;
+//	return it->str();
+//}
+
+
+
+//auto CParser::getByTagWithContext(string tag,string html) -> vector<string>{
+//
+//	//| <" + tag + "[^]*/>
+//	regex bodyRegExp("(<" + tag + " (.*)" + ">(.*)</" + tag + ">) | (<" + tag + "(.*)/>)");
+//
+//		string str = this->html;
+//		sregex_iterator it(str.begin(), str.end(), bodyRegExp);
+//		sregex_iterator it_end;
+//
+//		vector<string> s;
+//
+//		while (it != it_end){
+//	//cout << "|||New expression||||";
+//		//list.append(it->str());
+//			
+//	s.push_back(it->str());
+//			++it;
+//		}
+//
+//	return s;
+//}
