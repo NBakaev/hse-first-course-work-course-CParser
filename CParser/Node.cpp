95fea15d015c0ad69a7b4e7e19968001dcc06b66
Bakaev Nikita <ya@nbakaev.ru>
2014-03-26 14:48:32 +0400

    latest

--- CParser/Node.cpp
+++ CParser/Node.cpp
@@ -11,26 +11,18 @@ CNode::~CNode()
 {
 }
 
-
 auto CNode::setDeep(int deep)->void{
 	this->deep = deep;
-
 }
-
 
 auto CNode::setTagName(string tag)->void{
 	this->tagName = tag;
-
 }
 
 auto CNode::addChildren(CNode* childrenNode)->void{
 	this->childrens.push_back(childrenNode);
-
 }
 
-
 auto CNode::getAttributes()->vector<map<string, string>>{
-
 	return this->attributes;
-
 }
